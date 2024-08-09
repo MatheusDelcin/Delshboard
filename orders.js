d0273db05488b816f3f7aac36832b9b49a996737
MatheusDelcin <87344152+MatheusDelcin@users.noreply.github.com>
2024-08-08 22:59:00 -0300

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'Javascript Tutorial',
+        productNumber: '85743',
+        paymentStatus: 'Due',
+        status: 'Pendng'
+    },
+    {
+        productName: 'CSS Full Course',
+        productNumber: '97245',
+        paymentStatus: 'Refund',
+        status: 'Declined'
+    },
+    {
+        productName: 'Flex-Box Tutorial',
+        productNumber: '364520',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    }
+]
